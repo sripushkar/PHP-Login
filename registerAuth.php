97515180025efeba4b0147231ebf4c40599a2a6a
Sri Julapally <sripushkar@gmail.com>
2020-08-01 04:43:51 -0400

    added auth rules

--- registerAuth.php
+++ registerAuth.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $mysqli = new mysqli('localhost', 'root', 'root', 'loginSystem');
 
 $username = $_POST['username'];
@@ -11,15 +12,29 @@ $password = stripcslashes($password);
 $username = $mysqli->real_escape_string($username);
 $password = $mysqli->real_escape_string($password);
 
+if(strlen($username) < 6 || strlen($username) > 20){
+  $_SESSION['message'] = "Username must be between 6-20 characters.";
+  header("location: register.php");
+  die();
+}
+
+if(strlen($password) < 6){
+  $_SESSION['message'] = "Password must be at least 6 characters";
+  header("location: register.php");
+  die();
+}
+
 $duplicateUser = $mysqli->query("SELECT * FROM login WHERE username = '$username';");
 
 if($duplicateUser->num_rows != 0){
   $_SESSION['message'] = "This username already exists. Please try again";
   header("location: register.php");
+  die();
 }
 else{
-  $register = $mysqli->query('INSERT INTO login (username, password) VALUES ("$username", "$password");');
+  $register = $mysqli->query("INSERT INTO login (username, password) VALUES ('$username', '$password');");
   header("location: welcome.php");
+  die();
 }
 
  ?>
